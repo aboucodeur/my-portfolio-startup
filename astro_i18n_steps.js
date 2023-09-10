e7e468b732f08c6a7c119c17298ea4e4259ce50c
codeurabou <codeurabou123@gmail.com>
2023-09-10 18:59:40 +0000

    remove js file

--- astro_i18n_steps.js
+++
@@ -1,6 +0,0 @@
-//astro_i18n_steps.js
-
-/**
- * STEP 1 : Change folder structure default language like / and other /language
- * STEP 2 : Creact helper functions locate to src/i18n/(ui.ts and utils.ts)
- */
